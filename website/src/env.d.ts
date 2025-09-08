c5f7bde45045bf94d3b395b324b8c3aca9104602
Beast <idewagedebisma@gmail.com>
2025-09-08 15:35:51 +0800

    chore: revert tsconfig

--- website/src/env.d.ts
+++
@@ -1,7 +0,0 @@
-declare module '*.svg' {
-    type SVGAttributes = astroHTML.JSX.SVGAttributes & { title?: string };
-    type Props = SVGAttributes;
-
-    const Component: ((_props: Props) => any) & ImageMetadata;
-    export default Component;
-}
