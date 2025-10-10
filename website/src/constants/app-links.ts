d96878ccb8ec104ed830f584210090a6803b06e7
Beast <idewagedebisma@gmail.com>
2025-10-10 14:02:25 +0800

    feat: add invite referral page

---
+++ website/src/constants/app-links.ts
@@ -0,0 +1,5 @@
+export const APP_LINKS = {
+  android:
+    "https://play.google.com/store/apps/details?id=com.quantus.wallet&pcampaignid=web_share",
+  ios: "https://apps.apple.com/id/app/quantus/id6747967405",
+} as const;
