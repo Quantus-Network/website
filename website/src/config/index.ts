c00387d857a2beb8be73e321a3275afeb56e9f9c
Beast <idewagedebisma@gmail.com>
2025-10-21 13:03:27 +0800

    feat: add api fetch for leaderboard

--- website/src/config/index.ts
+++ website/src/config/index.ts
@@ -4,6 +4,7 @@ const env = {
   GA_ID: "G-JBZETNF6F1",
   API_URL: "https://api.quantus.com/api",
   GRAPHQL_URL: "https://quantu.se/graphql",
+  TASK_MASTER_URL: "http://localhost:3000/api",
 };
 
 export default env;
