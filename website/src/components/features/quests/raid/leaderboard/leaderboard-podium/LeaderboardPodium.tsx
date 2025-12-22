04a5be13bc42ecb17c07f2ca8629723473c438ea
Beast <idewagedebisma@gmail.com>
2025-12-22 16:31:45 +0800

    feat: update content of quest page

--- website/src/components/features/quests/raid/leaderboard/leaderboard-podium/LeaderboardPodium.tsx
+++ website/src/components/features/quests/raid/leaderboard/leaderboard-podium/LeaderboardPodium.tsx
@@ -13,31 +13,34 @@ const LeaderboardPodium = ({ locale }: LeaderboardPodiumProps) => {
   const secondRank = data?.data[1];
   const thirdRank = data?.data[2];
 
+  const unknownIdentity = "-";
+  const isLoading = status === "loading";
+
   return (
     <div className="flex flex-wrap justify-center gap-12 py-10 xl:grid xl:grid-cols-3">
       {error && <p className="text-red-500">{error.message}</p>}
 
       <WinnerPodium
         rank={2}
-        identity={secondRank?.raider.referral_code ?? "loading..."}
+        identity={secondRank?.raider.referral_code ?? unknownIdentity}
         impressions={secondRank?.total_impressions ?? 0}
-        isLoading={status === "loading"}
+        isLoading={isLoading}
         t={t}
       />
 
       <WinnerPodium
         rank={1}
-        identity={firstRank?.raider.referral_code ?? "loading..."}
+        identity={firstRank?.raider.referral_code ?? unknownIdentity}
         impressions={firstRank?.total_impressions ?? 0}
-        isLoading={status === "loading"}
+        isLoading={isLoading}
         t={t}
       />
 
       <WinnerPodium
         rank={3}
-        identity={thirdRank?.raider.referral_code ?? "loading..."}
+        identity={thirdRank?.raider.referral_code ?? unknownIdentity}
         impressions={thirdRank?.total_impressions ?? 0}
-        isLoading={status === "loading"}
+        isLoading={isLoading}
         t={t}
       />
     </div>
