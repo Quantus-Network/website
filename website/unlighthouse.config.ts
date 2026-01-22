861c7fd5a645c67430a6f2c6f3d1b6acc02ab483
Beast <idewagedebisma@gmail.com>
2026-01-22 16:45:39 +0800

    feat: add seo check CI

---
+++ website/unlighthouse.config.ts
@@ -0,0 +1,14 @@
+import { defineUnlighthouseConfig } from '@unlighthouse/cli'
+
+export default defineUnlighthouseConfig({
+  site: 'http://localhost:3000',
+  // Fail the build if SEO score is below 100
+  // Other categories (performance, accessibility, best-practices) will be audited but won't fail the build
+  ci: {
+    budget: {
+      seo: 100,
+    },
+  },
+  // Output path for reports
+  outputPath: './.unlighthouse',
+})
