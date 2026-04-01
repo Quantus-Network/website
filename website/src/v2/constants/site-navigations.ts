3567a82c33b1931564b3c432b47e8e668b7eeaa1
Beast <idewagedebisma@gmail.com>
2026-04-01 23:58:40 +0800

    hot deploy

--- website/src/v2/constants/site-navigations.ts
+++ website/src/v2/constants/site-navigations.ts
@@ -1,11 +1,10 @@
 export const SITE_NAVIGATIONS = [
-  { href: "/v2/technology/", label: "v2.navbar.technology" },
-  { href: "/v2/wallet/", label: "v2.navbar.wallet" },
-  { href: "/v2/about/", label: "v2.navbar.about" },
-  { href: "/v2/community/", label: "v2.navbar.community" },
+  { href: "/technology/", label: "v2.navbar.technology" },
+  { href: "/wallet/", label: "v2.navbar.wallet" },
+  { href: "/about/", label: "v2.navbar.about" },
+  { href: "/community/", label: "v2.navbar.community" },
   {
-    href: "/v2/quantum-risk-checker/",
+    href: "/quantum-risk-checker/",
     label: "v2.navbar.quantum_risk_checker",
   },
-  { href: "/v2/quan/", label: "v2.navbar.quan" },
 ] as const;
