6a575a5aec126027c6bc9c0a5ded62528ef42e83
Beast <idewagedebisma@gmail.com>
2026-05-04 13:13:17 +0800

    feat: update team section

---
+++ website/src/constants/teams.ts
@@ -0,0 +1,62 @@
+export const TEAMS = [
+    {
+        id: "christopher-smith",
+        name: "Christopher Smith",
+        role: "CEO",
+        social: {
+            platform: "X",
+            url: "https://x.com/YuviLightman",
+            username: "@YuviLightman",
+        }
+    },
+    {
+        id: "joe-mattia",
+        name: "Joseph Mattia",
+        role: "COO",
+        social: {
+            platform: "X",
+            url: "https://x.com/JoeMattia",
+            username: "@JoeMattia",
+        }
+    },
+    {
+        id: "nikolaus-hager",
+        name: "Nikolaus Hager",
+        role: "CTO",
+        social: {
+            platform: "X",
+            url: "https://x.com/n13",
+            username: "@n13",
+        }
+    },
+    {
+        id: "jarrod-jay-frankel",
+        name: "Jarrod Frankel",
+        role: "CMO",
+        social: {
+            platform: "X",
+            url: "https://x.com/j_in_marketing",
+            username: "@j_in_marketing",
+        }
+    },
+    {
+        id: "ethan-cemer",
+        name: "Ethan Cemer",
+        role: "Zero-Knowledge Engineer",
+        social: {
+            platform: "X",
+            url: "https://x.com/ethan_t_c_",
+            username: "@ethan_t_c_",
+        }
+    },
+    {
+        id: "jonathan-angle",
+        name: "Jonathan Angle",
+        role: "Communications Director",
+        social: {
+            platform: "X",
+            url: "https://x.com/defijangle",
+            username: "@defijangle",
+        }
+    },
+] as const;
