d4dbd05578f49bb2dbba45cad400c7f510a56945
Beast <idewagedebisma@gmail.com>
2026-05-08 16:43:02 +0800

    feat: add docs to navbar

--- website/src/constants/site-navigations.ts
+++ website/src/constants/site-navigations.ts
@@ -1,11 +1,23 @@
-export const SITE_NAVIGATIONS = [
+interface Navigation {
+
+  href: string;
+  label: string;
+  target?: string;
+  rel?: string;
+
+}
+
+export const SITE_NAVIGATIONS: Navigation[] = [
   { href: "/technology/", label: "navbar.technology" },
   { href: "/wallet/", label: "navbar.wallet" },
   { href: "/about/", label: "navbar.about" },
   { href: "/community/", label: "navbar.community" },
   {
+    href: "https://docs.quantus.com/", target: "_blank",
+    rel: "noopener noreferrer", label: "navbar.docs"
+  },
+  {
     href: "/quantum-risk-checker/",
     label: "navbar.quantum_risk_checker",
   },
-  // { href: "/quan/", label: "navbar.quan" },
 ] as const;
