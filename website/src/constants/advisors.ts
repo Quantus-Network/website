888c6e250457304b32ecfa6fdd952a6c325c985a
Beast <idewagedebisma@gmail.com>
2026-07-24 22:14:37 +0800

    feat: refactor and add new backer

---
+++ website/src/constants/advisors.ts
@@ -0,0 +1,32 @@
+export const ADVISORS = [
+  {
+    id: "raghav-gulati",
+    name: "Raghav Gulati",
+    role: "CEO, Coinlist",
+    social: {
+      platform: "X",
+      url: "https://x.com/rargulati",
+      username: "@rargulati",
+    },
+  },
+  {
+    id: "tom-howard",
+    name: "Tom Howard",
+    role: "Head of Strategy, Coinlist",
+    social: {
+      platform: "X",
+      url: "https://x.com/_TomHoward",
+      username: "@_TomHoward",
+    },
+  },
+  {
+    id: "arjun-khemani",
+    name: "Arjun Khemani",
+    role: "Writer · Crypto Advocate",
+    social: {
+      platform: "X",
+      url: "https://x.com/arjunkhemani",
+      username: "@arjunkhemani",
+    },
+  },
+] as const;
---
+++ website/src/constants/investors.ts
@@ -0,0 +1,32 @@
+export const INVESTORS = [
+  {
+    id: "balaji-srinivasan",
+    name: "Balaji Srinivasan",
+    role: "Founder, Network School",
+    social: {
+      platform: "X",
+      url: "https://x.com/balajis",
+      username: "@balajis",
+    },
+  },
+  {
+    id: "babak-nivi",
+    name: "Babak Nivi",
+    role: "Co-Founder, AngelList",
+    social: {
+      platform: "X",
+      url: "https://x.com/nivi",
+      username: "@nivi",
+    },
+  },
+  {
+    id: "mert-mumtaz",
+    name: "Mert Mumtaz ",
+    role: "CEO, Helius",
+    social: {
+      platform: "X",
+      url: "https://x.com/mert",
+      username: "@mert",
+    },
+  },
+] as const;
